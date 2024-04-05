6ff80a31f613491909c05625bf8a1cbfc7045085
Threnris <114936622+Threnris@users.noreply.github.com>
2024-04-06 02:21:05 +0800

    Update user.js

--- MCO3_APPDEV/routes/user.js
+++ MCO3_APPDEV/routes/user.js
@@ -1,9 +1,14 @@
 const express = require('express');
-
 const userController = require('../controllers/user');
 
 const router = express.Router();
-
 router.get('/user/:id', userController.getUser);
+
+router.get('/user/edit/:id', userController.getEditUser);
+
+router.post('/user/edit/:id', userController.editUser);
+
+
+router.post('/user/delete/:id', userController.deleteUser);
 
 module.exports = router;
